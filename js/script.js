f0bcdff54eb984fc32a059a9e5c296f03c69eaed
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-01-25 12:58:11 +0100

    Init html, collegamento bootstrap/f.a., steps js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*
+    STEPS:
+        Milestone 1
+            1. Ricreare la grafica dell'app in html/css (o bootstrap)
+            2. Aggiunta di tutti i contatti in maniera statica nella lsta
+            3. Aggiunta dei messaggi nel main (come nella foto esempio)
+                3A. Assegnazione delle classi sent o received ai messaggi, che regoleranno il loro comportamento nello schermo
+            4. Integrazione di vuejs
+            5. Aggiunta direttiva v-for per visualizzare dinamicamente i contatti nella lista
+        Milestone 2
+            1. Usare la direttiva v-for per visualizzare tutti i messaggi relativi ad un contatto nello schermo
+            2. Aggiungere un evento click ad ogni contatto per visualizzare i suoi messaggi a schermo
+        Milestone 3
+            1. Abilitare l'input text con v-model per scrivere i messaggi
+            2. Aggiungere all'input l'evento keyup.enter per inviare i messaggi
+            3. Intercettare l'invio dei messaggi per poter poi rendere una risposta 1 secondo dopo (timing function)
+        Milestone 4
+            1. Abilitare l'input nell'aside con v-model per cercare i contatti
+            2. Verranno visualizzati solo i contatti che contengono i caratteri digitati (funzione includes?)
+*/
